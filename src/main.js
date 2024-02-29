dccec64b7d79a05df936f9100c1eb7892622b0f7
zoeychenrui <zoeychen2013@gmail.com>
2024-02-28 23:35:23 -0500

    planets

--- src/main.js
+++ src/main.js
@@ -8,6 +8,66 @@ const MESSAGE_TOP = 1250 * FACTOR
 let stars = []
 let canvas2;
 
+
+let rotationSpeedX = 0.5;
+let rotationSpeedY = 0.5;
+
+// Adjust rotation speed based on mouse movement
+function mouseMoved() {
+  rotationSpeedX = map(mouseX, 0, width, -0.02, 0.02);
+  rotationSpeedY = map(mouseY, 0, height, -0.02, 0.02);
+}
+
+let userInput = ""; // Variable to store user input
+let inputField; // Input field element
+let floatingWords = []; // Array to hold floating words
+
+class Word {
+  constructor(x, y, z, text) {
+    this.x = x;
+    this.y = y;
+    this.z = z;
+    this.text = text;
+    this.speed = random(1, 3);
+  }
+
+  update() {
+    this.x += random(-1, 1) * this.speed;
+    this.y += random(-1, 1) * this.speed;
+    this.z += random(-1, 1) * this.speed;
+  }
+
+  display() {
+    push();
+    translate(this.x, this.y, this.z);
+    fill(255);
+    text(this.text, 0, 0);
+    pop();
+  }
+}
+
+let planets = []; // Array to hold planets
+
+class Planet {
+  constructor(x, y, z, size) {
+    this.x = x;
+    this.y = y;
+    this.z = z;
+    this.size = size;
+    this.color = color(random(255), random(255), random(255)); // Random color for each planet
+  }
+
+  display() {
+    push();
+    translate(this.x, this.y, this.z);
+    fill(this.color);
+    noStroke();
+    sphere(this.size); // Display planet as a sphere
+    pop();
+  }
+}
+
+
 function setup() {
   let canvas = createCanvas(windowWidth, windowHeight, WEBGL)
   canvas2 = createGraphics(windowWidth, 1920 * FACTOR );
@@ -44,6 +104,7 @@ function setup() {
     
     points.push({x:x, y:y, z:z})
   }
+
   
 }
 
@@ -54,8 +115,8 @@ function draw() {
   orbitControl();
   t =  frameCount/50;
 
-  rotateX(t/2)
-  rotateY(t/2)
+  rotateX(frameCount * rotationSpeedX)
+  rotateY(frameCount * rotationSpeedY)
   noFill();
 
   for (a = t; a < PI+t; a += PI / 6) {
@@ -94,7 +155,16 @@ function draw() {
   Global.time += deltaTime / 1000
   
   drawStars()
+ 
+  for (let word of floatingWords) {
+    word.update();
+    word.display();
+  }
+  for (let planet of planets) {
+    planet.display();
+  }
 }
+
 
 
 function drawStars() {
@@ -102,7 +172,25 @@ function drawStars() {
     star.draw()
   })
 }
+function mouseClicked() {
+  // Create a new planet at the position of the mouse click
+  let x = mouseX - width / 2;
+  let y = mouseY - height / 2;
+  let z = random(-500, 500);
+  let size = random(10, 50); // Random size for each planet
+  let planet = new Planet(x, y, z, size);
+  planets.push(planet);
+}
 
-
-
-
+function updateUserInput() {
+  // Update userInput variable when user presses Enter
+  userInput = inputField.value();
+  inputField.value(""); // Clear input field after submission
+  
+  // Create a new floating word instance for the user input
+  let x = random(-width / 2, width / 2);
+  let y = random(-height / 2, height / 2);
+  let z = random(-500, 500);
+  let word = new Word(x, y, z, userInput);
+  floatingWords.push(word);
+}
